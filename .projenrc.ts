e850ec1e06526f04a01fb5b17400540ce04719f0
Court Schuett <schuettc@amazon.com>
2023-09-06 15:01:58 -0500

    fix: update projen

---
+++ .projenrc.ts
@@ -0,0 +1,108 @@
+const { awscdk } = require('projen');
+const { JobPermission } = require('projen/lib/github/workflows-model');
+const { UpgradeDependenciesSchedule } = require('projen/lib/javascript');
+
+const AUTOMATION_TOKEN = 'PROJEN_GITHUB_TOKEN';
+
+const project = new awscdk.AwsCdkTypeScriptApp({
+  cdkVersion: '2.70.0',
+  defaultReleaseBranch: 'main',
+  name: 'amazon-chime-sdk-call-analytics-recording',
+  license: 'MIT-0',
+  author: 'Court Schuett',
+  copyrightOwner: 'Amazon.com, Inc.',
+  authorAddress: 'https://aws.amazon.com',
+  projenrcTs: true,
+  appEntrypoint: 'amazon-chime-sdk-call-analytics-recording.ts',
+  eslintOptions: { ignorePatterns: ['src/resources/server/assets/site/**'] },
+  depsUpgradeOptions: {
+    ignoreProjen: false,
+    workflowOptions: {
+      labels: ['auto-approve', 'auto-merge'],
+      schedule: UpgradeDependenciesSchedule.WEEKLY,
+    },
+  },
+  autoApproveOptions: {
+    secret: 'GITHUB_TOKEN',
+    allowedUsernames: ['schuettc'],
+  },
+  deps: [
+    'dotenv',
+    'cdk-amazon-chime-resources',
+    '@aws-cdk/aws-apigatewayv2-alpha',
+    '@aws-cdk/aws-apigatewayv2-integrations-alpha',
+    '@aws-sdk/client-apigatewaymanagementapi',
+    '@aws-sdk/client-dynamodb',
+    '@aws-sdk/client-transcribe',
+    'aws-lambda',
+    '@types/aws-lambda',
+    '@aws-sdk/client-sagemaker',
+    'graphql-request',
+    'graphql',
+  ],
+  autoApproveUpgrades: true,
+  projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
+});
+
+const upgradeSite = project.github.addWorkflow('upgrade-site');
+upgradeSite.on({ schedule: [{ cron: '0 5 * * 1' }], workflowDispatch: {} });
+upgradeSite.addJobs({
+  upgradeSite: {
+    runsOn: ['ubuntu-latest'],
+    name: 'upgrade-site',
+    permissions: {
+      actions: JobPermission.WRITE,
+      contents: JobPermission.READ,
+      idToken: JobPermission.WRITE,
+    },
+    steps: [
+      { uses: 'actions/checkout@v3' },
+      {
+        name: 'Setup Node.js',
+        uses: 'actions/setup-node@v3',
+        with: {
+          'node-version': '16',
+        },
+      },
+      {
+        run: 'yarn install --check-files --frozen-lockfile',
+        workingDirectory: 'src/resources/server/assets/site',
+      },
+      {
+        run: 'yarn upgrade',
+        workingDirectory: 'src/resources/server/assets/site',
+      },
+      {
+        name: 'Create Pull Request',
+        uses: 'peter-evans/create-pull-request@v4',
+        with: {
+          'token': '${{ secrets.' + AUTOMATION_TOKEN + ' }}',
+          'commit-message': 'chore: upgrade site',
+          'branch': 'auto/projen-upgrade',
+          'title': 'chore: upgrade site',
+          'body': 'This PR upgrades site',
+          'labels': 'auto-merge, auto-approve',
+          'author': 'github-actions <github-actions@github.com>',
+          'committer': 'github-actions <github-actions@github.com>',
+          'signoff': true,
+        },
+      },
+    ],
+  },
+});
+
+const common_exclude = [
+  '.yalc',
+  'cdk.out',
+  'cdk.context.json',
+  'yarn-error.log',
+  'dependabot.yml',
+  '.DS_Store',
+];
+
+project.addTask('launch', {
+  exec: 'yarn && yarn projen && yarn build && yarn cdk bootstrap && yarn cdk deploy --require-approval never',
+});
+
+project.gitignore.exclude(...common_exclude);
+project.synth();
